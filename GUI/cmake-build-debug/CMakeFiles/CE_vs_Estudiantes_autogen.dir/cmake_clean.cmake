446f91b8bed069f165b7f7693e10d985f124f729
Calquito <39011234+Calquito@users.noreply.github.com>
2019-11-02 03:04:53 -0600

    Add files via upload

---
+++ GUI/cmake-build-debug/CMakeFiles/CE_vs_Estudiantes_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CE_vs_Estudiantes_autogen/mocs_compilation.cpp"
+  "CMakeFiles/CE_vs_Estudiantes_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CE_vs_Estudiantes_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
